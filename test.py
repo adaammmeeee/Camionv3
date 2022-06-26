cd07a9471ef6b0c5db6461178c729da7e266de80
adaammmeeee <adame.abdelaziz@laposte.net>
2022-06-26 15:01:50 +0200

    implementation graphique continue

--- test.py
+++ test.py
@@ -1,41 +1,52 @@
+from tkinter import *
 import tkinter
 import tkintermapview
+from tkinter import messagebox
 from matplotlib import pyplot as plt
 
 
-def test():
-    print("Coucou")
+def gestionnaire_entrepot(numero):
+    entrepot = tkinter.Tk()
+    entrepot.geometry(f"{300}x{300}")
+    entrepot.title("Gestionnaire de l'entrepot " + numero)
+    # Création de la liste
+    nombre_camion = 8
+    liste = Listbox(entrepot, width=30, height=nombre_camion)
+    for i in range(nombre_camion):
+        liste.insert(i, "Camion " + str(i))
+    liste.place(x=0, y=0)
 
-def check_list():
-    try:
-        s = liste.selection_get()
-        requete = Tk()
-        requete.title("Requête de l'" + s)
-        requete.geometry("300x300")
-        label_title = Label(requete, text="Requête")
-        label_title.pack()
-    except:
-        print('no selection')
+    def check_list():
+        try:
+            s = liste.selection_get()
+            print(s)
+        except:
+            print("Aucune requête n'a été sélectionnée")
+
+    but = Button(entrepot, text="Voir le trajet", command=check_list)
+    but.place(x=0, y=16*nombre_camion+2)
 
 
-def create_circle(x, y, r, canvasName, identrepot):
-    canvasName.create_oval(x-r, y-r, x+r, y+r, fill="#C5C600")
-    w.create_text(x, y, text=identrepot, fill="black",
-                  font=('Helvetica' + str(int(r/2))+'bold'))
+def click_airport_marker_event(marker):
+    values = ['Voir requêtes', 'Voir trajet']
+    gestionnaire_entrepot(marker.text[-1])
 
 
-# create tkinter window
+# Création d'une fenêtre Tkinter
 root_tk = tkinter.Tk()
 root_tk.geometry(f"{800}x{600}")
 root_tk.title("map_view_example.py")
 
-# create map widget
-map_widget = tkintermapview.TkinterMapView(root_tk, width=800, height=600, corner_radius=0)
+
+# Création de la map
+map_widget = tkintermapview.TkinterMapView(
+    root_tk, width=800, height=600, corner_radius=0)
 map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-map_widget.set_position(46.8734246,2.6397020)  # Paris, France
+map_widget.set_position(46.8734246, 2.6397020)  # Paris, France
 map_widget.set_zoom(6.1)
 
-#Lecture du fichier pour récupérer la position de tous les agents qu'on stockera dans la variable position
+
+# Lecture du fichier pour récupérer la position de tous les agents qu'on stockera dans la variable position
 positions = []
 f = open("position_sites.csv", "r")
 lignes = f.readlines()
@@ -44,27 +55,18 @@ for line in lignes:
     tuple = line.partition(";")
     positions.append(tuple)
 
+
+# Placement de tous les entrepots sur la carte
 cpt = 0
 marker_list = []
 for position in positions:
-    marker = map_widget.set_marker(float(position[0]), float(position[2]), text="Entrepot " + str(cpt),marker_color_outside = "#A9A9A9", marker_color_circle = "#808080", font = 10)
+    marker = map_widget.set_marker(float(position[0]), float(position[2]), command=click_airport_marker_event, text="Entrepot " + str(
+        cpt), text_color="black", marker_color_outside="#A9A9A9", marker_color_circle="#808080", font=10)
     marker_list.append(marker)
     cpt += 1
-    
-path_1 = map_widget.set_path([marker_list[0].position, marker_list[1].position], color="#FF0000", command = test())
-
-root_tk.mainloop() 
 
 
-
-
-
-
-
-
-
-
-
+root_tk.mainloop()
 
 
 """
@@ -84,16 +86,6 @@ w.pack(expand=YES, fill=BOTH)
     
 plt.scatter(x=df['Longitude'], y=df['Latitude'])
 plt.show()
-
-#On a chargé un tuple contenant nos positions maintenant on va créer nos entrepots aux positions de ces tuples 
-i = 0
-for position in positions:
-    x = (float(position[0]) - 40) * ((largeur)/10)-150 #à expliquer
-    y = (float(position[2])+3) * (hauteur/11) 
-    create_circle(x, y, 10, w, str(i))
-    i += 1
-
-    
 
 
 
